3dcb24776aa0d429ce25780154a45c0d545d2767
Bao Hengtao <baohengtao@users.noreply.github.com>
2023-02-21 20:32:56 +0800

    add sina_clean option to tidy_photo_in_album

--- photosinfo/script.py
+++ photosinfo/script.py
@@ -5,7 +5,9 @@ from typer import Typer
 from photosinfo import console
 from photosinfo.photosinfo import (
     add_photo_to_album,
-    update_artist, update_table)
+    update_artist,
+    update_table
+)
 
 app = Typer(
     pretty_exceptions_enable=True,
@@ -14,7 +16,7 @@ app = Typer(
 
 
 @app.command()
-def tidy_photo_in_album(new_artist: bool = False):
+def tidy_photo_in_album(new_artist: bool = False, sina_clean: bool = False):
     photosdb = PhotosDB()
     console.log('update table...')
     update_table(photosdb)
@@ -22,3 +24,31 @@ def tidy_photo_in_album(new_artist: bool = False):
     console.log('add photo to album...')
     photoslib = PhotosLibrary()
     add_photo_to_album(photosdb, photoslib)
+    if sina_clean:
+        sina_db_clean()
+
+
+def sina_db_clean():
+    import questionary
+    from sinaspider.model import Artist, User, UserConfig, Weibo
+
+    from photosinfo.model import Photo
+    if not questionary.confirm('Have you backup database to rpi?').ask():
+        console.log('Backup first, bye!')
+        return
+    if not questionary.confirm('Have you put all photos to Photos.app?').ask():
+        console.log('put them to Photos.app first, bye!')
+        return
+    photos = (Photo.select()
+              .where(Photo.image_supplier_name == "Weibo")
+              .where(Photo.image_unique_id.is_null(False)))
+    photo_bids = {p.image_unique_id for p in photos}
+    console.log(f'{len(photo_bids)} weibos in photos.app\n'
+                f'{len(Weibo)} weibos in sina database')
+    del_count = Weibo.delete().where(Weibo.bid.not_in(photo_bids)).execute()
+    console.log(f'{del_count} weibos have been deleted\n'
+                f'{len(Weibo)} weibos left in sina database')
+    uids = {u.user_id for u in UserConfig} | {u.user_id for u in Artist}
+    del_count = User.delete().where(User.id.not_in(uids)).execute()
+    console.log(f'{del_count} users have been deleted')
+    console.log('Done!')
