f0239653379afb8f9b09a165da0736882506a7c3
Bao Hengtao <baohengtao@users.noreply.github.com>
2023-10-31 18:12:16 +0800

    refine get_photo2album

--- photosinfo/photosinfo.py
+++ photosinfo/photosinfo.py
@@ -55,26 +55,29 @@ class GetAlbum:
     def get_photo2album(self, supplier, uid, photos):
         supplier = supplier.lower() if supplier else 'no_supplier'
         assert supplier != 'weibosaved'
-        if supplier == 'weiboliked':
+        if supplier in ['weiboliked', 'weibosavedfail']:
             if (pic_num := len(photos)) > 50:
                 liked_by = photos[0].title.split('⭐️')[1]
                 artist = photos[0].artist
                 album = '_'.join((liked_by, artist))
             else:
                 album = str(math.ceil(pic_num/10)*10)
-            for p in photos:
-                if uid in self.supplier_dict['Weibo']:
-                    self.photo2album[p] = ('weibosaved', 'done', album)
-                elif uc := UserConfig.get_or_none(user_id=uid):
-                    if uc.weibo_fetch_at:
-                        assert uc.weibo_fetch is True
-                        self.photo2album[p] = ('weibosaved', 'fetched', album)
-                    else:
-                        self.photo2album[p] = ('weibosaved', 'added', album)
-                elif photos[0].favorite:
-                    self.photo2album[p] = ('weiboliked', "tbd", album)
+            if uid in self.supplier_dict['Weibo']:
+                folder = ('weibosaved', 'done', album)
+            elif uc := UserConfig.get_or_none(user_id=uid):
+                if uc.weibo_fetch_at:
+                    assert uc.weibo_fetch is True
+                    folder = ('weibosaved', 'fetched', album)
                 else:
-                    self.photo2album[p] = ('weiboliked', None, album)
+                    folder = ('weibosaved', 'added', album)
+            elif photos[0].favorite:
+                folder = ('weiboliked', "tbd", album)
+            else:
+                folder = ('weiboliked', None, album)
+            if supplier == 'weibosavedfail':
+                folder = (supplier,) + folder[1:]
+            for p in photos:
+                self.photo2album[p] = folder
 
         elif supplier not in kls_dict:
             assert uid is None
@@ -91,28 +94,22 @@ class GetAlbum:
                     if supplier != 'weibo':
                         console.log(f'{username}({supplier}): discard {fld}',
                                     style='warning')
-                first_folder = 'weibo'
                 artist = self.username_in_weibo[username]
-            else:
-                first_folder = supplier
-            fld_lock = False
-            if fld := artist.folder:
-                if fld.startswith('recent') or fld == 'new':
-                    fld_lock = True
-            if not fld_lock and username in self.username_in_insweibo:
-                second_folder = 'ins-super' if artist.folder == 'super' else 'ins'
-            else:
-                second_folder = artist.folder
-            if second_folder:
-                if artist.photos_num < 50 and not fld_lock:
-                    album = 'small'
-                else:
-                    album = username
+            first_folder = supplier
+            second_folder = artist.folder
+            if username in self.username_in_insweibo:
+                first_folder = 'insweibo'
+                if artist.folder is None:
+                    second_folder = 'ins'
 
-            elif first_folder in ['instagram', 'twitter']:
-                album = 'small' if artist.photos_num < 30 else username
-            else:
-                for flag in [200, 100, 50]:
+            album = 'small' if artist.photos_num < 30 else username
+            if second_folder:
+                if artist.photos_num < 30:
+                    if (second_folder.startswith('recent')
+                            or second_folder == 'new'):
+                        album = username
+            elif first_folder == 'weibo':
+                for flag in [100, 50, 25]:
                     if artist.photos_num >= flag:
                         second_folder = str(flag)
                         album = username
@@ -125,6 +122,9 @@ class GetAlbum:
                             break
                     else:
                         album = 'problem'
+            if second_folder == 'super':
+                second_folder = None
+
             for p in photos:
                 if p.artist != username:
                     self.photo2album[p] = (first_folder, None, 'problem')
@@ -152,6 +152,13 @@ class GetAlbum:
                 supplier not in ['weiboliked', 'weibosaved'] and
                     p.date > pendulum.now().subtract(months=2)):
                 album_info[('refresh',)].add(p.uuid)
+        for alb_path in album_info.copy().keys():
+            *folder,  album = alb_path
+            for dup in ['root', 'all']:
+                if album == dup:
+                    break
+                elif album_info[alb_path] == album_info[(*folder, dup)]:
+                    album_info.pop((*folder, dup))
 
         if self.photosdb and (keywords := self.photosdb.keywords):
             query = QueryOptions(keyword=keywords)
@@ -166,8 +173,10 @@ class GetAlbum:
         album_info[('wide',)] = {
             p.uuid for p in self.photosdb.photos()
             if p.width > p.height and p.uuid in album_info[('favorite',)]}
+        album_info = {k: v for k, v in album_info.items() if v}
         album_info = OrderedDict(sorted(album_info.items(), key=lambda x: len(
             x[1]) if 'favorite' not in x[0] else 9999999))
+
         return album_info
 
     def create_album(self, recreating=True):
@@ -198,7 +207,7 @@ class GetAlbum:
                         console.log(model_to_dict(unexpected_photo))
                         console.log(f'the unexpectedphoto will added to album'
                                     f':=>{self.photo2album[unexpected_photo]}')
-                        if recreating:
+                        if recreating or len(album_uuids) < 2000:
                             console.log(f'Recreating {alb_path}')
                             self.photoslib.delete_album(
                                 self.photoslib.album(uuid=alb.uuid))
